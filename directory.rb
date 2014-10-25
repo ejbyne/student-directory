83e470582a8cc051a92d643e6c9ef7147f317b32
ed <ejbyne@gmail.com>
2014-10-25 13:21:29 +0200

    Completed exercises 1 and 2

--- directory.rb
+++ directory.rb
@@ -1,7 +1,7 @@
 #Let's put all students into an array
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  print "Please enter the names of the students\n"
+  print "To finish, just hit return twice\n"
   # create an empty array
   students = []
   # get the first name
@@ -10,7 +10,7 @@ def input_students
   while !name.empty? do
     #add the student hash to the array
     students << {:name => name, :cohort => :october}
-    puts "Now we have #{students.length} students"
+    print "Now we have #{students.length} students\n"
     #get another name from the user
     name = gets.chomp
   end
@@ -19,21 +19,21 @@ def input_students
 end
 
 def print_header
-  puts "The students of my cohort at Makers Academy"
-  puts "------------------"
+  print "The students of my cohort at Makers Academy\n"
+  print "------------------\n"
 end
 
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+def print_list(students)
+  students.each_with_index do |student, i|
+    print "#{i+1}. #{student[:name]} (#{student[:cohort]} cohort)\n"
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.length} great students"
+  print "Overall, we have #{students.length} great students\n"
 end
 
 students = input_students
 print_header
-print(students)
+print_list(students)
 print_footer(students)

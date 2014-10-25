f23a8d3961c25f50a9db50db0dff85773a2d9700
ed <ejbyne@gmail.com>
2014-10-25 10:48:44 +0200

    Added hashes

--- directory.rb
+++ directory.rb
@@ -1,25 +1,25 @@
 #Let's put all students into an array
 students = [
-  "Ed B",
-  "Ben H",
-  "Rich L",
-  "Chris H",
-  "Victoria S",
-  "Ella S",
-  "Izzy M",
-  "Nick D",
-  "Alex B",
-  "Giorgia A",
-  "Chris B",
-  "Dale S",
-  "Alan B",
-  "Tim S",
-  "Mishal I",
-  "Pavel R",
-  "Simon L",
-  "Clare T",
-  "Claudia B",
-  "Ben C"
+  {:name => "Ed B", :cohort => :october},
+  {:name => "Ben H", :cohort => :october},
+  {:name => "Rich L", :cohort => :october},
+  {:name => "Chris H", :cohort => :october},
+  {:name => "Victoria S", :cohort => :october},
+  {:name => "Ella S", :cohort => :october},
+  {:name => "Izzy M", :cohort => :october},
+  {:name => "Nick D", :cohort => :october},
+  {:name => "Alex B", :cohort => :october},
+  {:name => "Giorgia A", :cohort => :october},
+  {:name => "Chris B", :cohort => :october},
+  {:name => "Dale S", :cohort => :october},
+  {:name => "Alan B", :cohort => :october},
+  {:name => "Tim S", :cohort => :october},
+  {:name => "Mishal I", :cohort => :october},
+  {:name => "Pavel R", :cohort => :october},
+  {:name => "Simon L", :cohort => :october},
+  {:name => "Clare T", :cohort => :october},
+  {:name => "Claudia B", :cohort => :october},
+  {:name => "Ben C", :cohort => :october}
 ]
 
 def print_header
@@ -27,9 +27,9 @@ def print_header
   puts "------------------"
 end
 
-def print_names(names)
-  names.each do |name|
-    puts name
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
@@ -38,5 +38,5 @@ def print_footer(names)
 end
 
 print_header
-print_names(students)
+print(students)
 print_footer(students)

ae9a67276129c028d55da0ef097506aae1934bdc
ed <ejbyne@gmail.com>
2014-10-25 13:04:32 +0200

    Added interactivity

--- directory.rb
+++ directory.rb
@@ -1,26 +1,22 @@
 #Let's put all students into an array
-students = [
-  {:name => "Ed B", :cohort => :october},
-  {:name => "Ben H", :cohort => :october},
-  {:name => "Rich L", :cohort => :october},
-  {:name => "Chris H", :cohort => :october},
-  {:name => "Victoria S", :cohort => :october},
-  {:name => "Ella S", :cohort => :october},
-  {:name => "Izzy M", :cohort => :october},
-  {:name => "Nick D", :cohort => :october},
-  {:name => "Alex B", :cohort => :october},
-  {:name => "Giorgia A", :cohort => :october},
-  {:name => "Chris B", :cohort => :october},
-  {:name => "Dale S", :cohort => :october},
-  {:name => "Alan B", :cohort => :october},
-  {:name => "Tim S", :cohort => :october},
-  {:name => "Mishal I", :cohort => :october},
-  {:name => "Pavel R", :cohort => :october},
-  {:name => "Simon L", :cohort => :october},
-  {:name => "Clare T", :cohort => :october},
-  {:name => "Claudia B", :cohort => :october},
-  {:name => "Ben C", :cohort => :october}
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student hash to the array
+    students << {:name => name, :cohort => :october}
+    puts "Now we have #{students.length} students"
+    #get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
 
 def print_header
   puts "The students of my cohort at Makers Academy"
@@ -33,10 +29,11 @@ def print(students)
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.length} great students"
+def print_footer(students)
+  puts "Overall, we have #{students.length} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)

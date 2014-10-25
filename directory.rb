ddf0fed480793ebfa9a22d4e9f917d6329ed7581
ed <ejbyne@gmail.com>
2014-10-25 20:08:29 +0200

    Added load option

--- directory.rb
+++ directory.rb
@@ -11,6 +11,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -22,6 +23,8 @@ def process(selection)
   	  show_students
     when "3"
       save_students
+    when "4"
+      load_students
   	when "9"
   	  exit
   	else
@@ -32,9 +35,19 @@ def process(selection)
 def save_students
   file = File.open("students.csv", "w")
   @students.each do |student|
-  	student_data = [student[:name], student [:cohort]]
+  	student_data = [student[:name], student[:cohort], student[:hobbies], student[:birth_country], student[:height]]
   	csv_line = student_data.join(", ")
   	file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  @students = []
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  	name, cohort, hobbies, birth_country, height = line.chomp.split(", ")
+  	@students << {:name => name, :cohort => cohort.to_sym, :hobbies => hobbies, :birth_country => birth_country, :height => height}
   end
   file.close
 end
